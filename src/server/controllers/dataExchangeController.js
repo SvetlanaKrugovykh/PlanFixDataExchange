e9423c969a3a5ff448828c21cefe40efc69b6dc3
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2024-04-29 17:28:04 +0200

    pass the message to the commit command

--- src/server/controllers/dataExchangeController.js
+++ src/server/controllers/dataExchangeController.js
@@ -1,16 +1,18 @@
+
 const HttpError = require('http-errors')
 const { sendReqToDB } = require('../services/dataExchangeService')
 
 module.exports.getDataFromDB = async function (request, _reply) {
   const { reqType, text } = request.body
   const message = await sendReqToDB(reqType, text)
+  const unescapedString = message.replace(/\\"/g, '"').replace(/\\\\/g, '\\').replace(/\\r\\n/g, '\n')
 
   if (!message) {
     throw new HttpError[501]('Command execution failed')
   }
 
   return {
-    message: `Sign on ${message}`
+    unescapedString
   }
 }
 
