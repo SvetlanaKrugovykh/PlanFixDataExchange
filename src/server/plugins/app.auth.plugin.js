87c0efafe2262e080ca80cd45562232653ab329b
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2024-05-09 14:19:25 +0200

    Added some security features to the app.auth.plugin.js

--- src/server/plugins/app.auth.plugin.js
+++ src/server/plugins/app.auth.plugin.js
@@ -1,32 +1,32 @@
 const fp = require('fastify-plugin')
+const ipRangeCheck = require('ip-range-check')
+const authService = require('../services/authService')
 require('dotenv').config()
 //const authService = require('../services/authService')
 
 const allowedIPAddresses = process.env.API_ALLOWED_IPS.split(',')
+const allowedSubnets = process.env.API_ALLOWED_SUBNETS.split(',')
 
 const restrictIPMiddleware = (req, reply, done) => {
   const clientIP = req.ip
-  if (!allowedIPAddresses.includes(clientIP)) {
+  if (!allowedIPAddresses.includes(clientIP) && !ipRangeCheck(clientIP, allowedSubnets)) {
     console.log(`${new Date()}: Forbidden IP: ${clientIP}`)
     reply.code(403).send('Forbidden')
   } else {
+    console.log(`${new Date()}: Allowed IP: ${clientIP}`)
     done()
   }
 }
 
 async function authPlugin(fastify, _ = {}) {
   fastify.decorateRequest('auth', null)
-
   fastify.addHook('onRequest', restrictIPMiddleware)
-
   fastify.addHook('onRequest', async (request, _) => {
     const { authorization } = request.headers
-
     request.auth = {
       token: null,
       clientId: null
     }
-
     if (authorization) {
       try {
         //const decoded = await authService.checkAccessToken(authorization)
@@ -36,6 +36,10 @@ async function authPlugin(fastify, _ = {}) {
         }
       } catch (e) {
         console.log(e)
+      }
+      const decodedToken = await authService.checkAccessToken(authorization)
+      if (decodedToken) {
+        console.log('decodedToken:', decodedToken)
       }
     }
   })
